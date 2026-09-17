0616944b9c91433d2478fcc7a0c37cca3be64660
handix <114307107+handix-dev@users.noreply.github.com>
2026-09-17 14:04:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -442,7 +442,7 @@ function genererListeJoueurs(equipe, joueurs) {
       </div>
       <div class="score-container" style="display: flex; align-items: baseline; gap: 4px;">
         <span class="match-score">${nbButs}</span>
-        <span style="font-size: 11px; font-weight: 500; color: #888;">${labelButs}</span>
+        <span style="font-size: 11px; font-weight: 500; color: var(--primary);">${labelButs}</span>
       </div>
     </div>
     `;
